703221d714c85a53748c24eda598b6e1336a4a2d
Simon Delberghe <simon.delberghe@etudiant.univ-lille1.fr>
2015-11-19 11:54:39 +0100

    Etat du projet au 19/11

--- 7/mshell.c
+++ 7/mshell.c
@@ -6,6 +6,7 @@
 #include <signal.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <assert.h>
 
 #include "cmd.h"
 #include "sighandlers.h"
--- 7/pipe.c
+++ 7/pipe.c
@@ -12,32 +12,64 @@
 
 
 void do_pipe(char *cmds[MAXCMDS][MAXARGS], int nbcmd, int bg) {
-    int fd[2];
+    
+    int fd = malloc(sizeof(int) * (nbcmd -1) * 2);
+
+    int i;
     assert(pipe(fd) != -1);
 
+    for (int i = 0; i < nbcmd; ++i) {
+        assert(pipe(fd[i]) != -1);
+    }
+
+
+    printf("%s\n", cmds[1][0]);
+
     switch(fork()) {
-    	case -1: exit(EXIT_FAILURE);
-    	case 0:
-    		close(fd[0]);
-    		dup2(fd[1], STDOUT_FILENO);
-    		close(fd[1]);
-    		execvp(cmds[0][0], cmds[0]);
+        case -1: 
+            exit(EXIT_FAILURE);
+        case 0:
+            close(fd[0]);
+            dup2(fd[1], STDOUT_FILENO);
+            close(fd[1]);
+            execvp(cmds[0][0], cmds[0]);
+    }
+
+    if (nbcmd > 2) {
+        for (i = 1; i < nbcmd; i++) {
+            
+            switch(fork()) {
+                case -1: 
+                    exit(EXIT_FAILURE);
+                case 0:
+                    dup2(fd[0], STDIN_FILENO);
+                    dup2(fd[1], STDOUT_FILENO);
+                    close(fd[0]);
+                    close(fd[1]);
+                    execvp(cmds[i][0], cmds[i]);
+            }
+        }
     }
 
     switch(fork()) {
-    	case -1: exit(EXIT_FAILURE);
+    	case -1: 
+            exit(EXIT_FAILURE);
     	case 0:
     		close(fd[1]);
     		dup2(fd[0], STDIN_FILENO);
     		close(fd[0]);
-    		execvp(cmds[1][0], cmds[1]);
+    		execvp(cmds[nbcmd][0], cmds[nbcmd]);
     }
 
     close(fd[0]);
     close(fd[1]);
 
-    wait(NULL);
-    wait(NULL);
+
+    if (!bg) {
+        for (i = 0; i < nbcmd; ++i) {
+            wait(NULL);
+        }
+    }
 
     return;
 }
